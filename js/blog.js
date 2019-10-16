18b827e662106cc5d164aec5f0901d9136ebbec4
JC-Vasquez <00094918@uca.edu.sv>
2019-10-16 15:58:21 -0600

    prueba de pagina

---
+++ js/blog.js
@@ -0,0 +1,42 @@
+let ocultar =() =>{
+    document.getElementById("todo").style.display = "none";
+}
+
+let mostrar_1 =() =>{
+    document.getElementById("dia_1").style.display = "block";
+    document.getElementById("dia_2").style.display = "none";
+    document.getElementById("dia_3").style.display = "none";
+    document.getElementById("dia_4").style.display = "none";
+    document.getElementById("dia_5").style.display = "none";
+};
+
+let mostrar_2 =() =>{
+    document.getElementById("dia_2").style.display = "block";
+    document.getElementById("dia_1").style.display = "none";
+    document.getElementById("dia_3").style.display = "none";
+    document.getElementById("dia_4").style.display = "none";
+    document.getElementById("dia_5").style.display = "none";
+};
+let mostrar_3 =() =>{
+    document.getElementById("dia_3").style.display = "block";
+    document.getElementById("dia_2").style.display = "none";
+    document.getElementById("dia_1").style.display = "none";
+    document.getElementById("dia_4").style.display = "none";
+    document.getElementById("dia_5").style.display = "none";
+};
+
+let mostrar_4 =() =>{
+    document.getElementById("dia_4").style.display = "block";
+    document.getElementById("dia_2").style.display = "none";
+    document.getElementById("dia_3").style.display = "none";
+    document.getElementById("dia_1").style.display = "none";
+    document.getElementById("dia_5").style.display = "none";
+};
+
+let mostrar_5 =() =>{
+    document.getElementById("dia_5").style.display = "block";
+    document.getElementById("dia_2").style.display = "none";
+    document.getElementById("dia_3").style.display = "none";
+    document.getElementById("dia_4").style.display = "none";
+    document.getElementById("dia_1").style.display = "none";
+};
